8d25db1ff4a702dfc3271084b3cccf38369e711f
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-06-26 10:57:35 +0700

    membuat dasar dari phon

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,134 +1,146 @@
-#include <iostream>
- using namespace std;
+#include<iostream>
+using namespace std;
 
- class Node
- {
-public: 
+class node{
+public:
     int info;
-    Node*leftchild;
-    Node*rightchild;
+    node *leftchild;
+    node *rightchild;
 
-    // Constructor for the node class
-    Node()
-    {
-        leftchild = nullptr; // Initialize left child to null
-        rightchild = nullptr; // Intialize right child to null
+    //constructor for the node class
+    
+};
+
+class BinaryTree
+{
+
+public:
+    node *ROOT;
+    BinaryTree(){
+        ROOT = nullptr; //initializing ROOT to null
     }
- };
- 
- class BinaryTree
- {
-    Node*ROOT;
-
-    BinarytTree()
-    {
-        ROOT = nullptr; // Intializing ROOT to null 
-    }
-
-    void insert()
-    {
+    void insert(){
         int x;
-        cout << "Masukkan nilai; ";
+        cout << "Masukkan nilai ";
         cin >> x;
+        
+        //step 1: Allocate memory for the new node
+        node *newnode = new node();
 
-        // Step 1; Allocate memory for the new node
-        Node*newNode = new Node();
+        //step 2 : assing the value to the data field of new node 
+        newnode->info = x;
 
-        // step 2:Assign value to the data field of new node
-        newNode->info = x;
+        //step 3: make the left and right child of the new node point to null 
+        newnode->leftchild = nullptr;
+        newnode->rightchild = nullptr;
 
-        // step 3: Make the lift and right child of the new node point to NULL
-        newNode->leftchild = nullptr;
-        newNode->rightchild = nullptr;
+        //step 4: locate the new node which will be the parent of the node to bw inserted
+        node *parent = nullptr;
+        node *currentnode = nullptr;
+        search(x, parent, currentnode);
 
-        // step 4: Locate the node which will be the parent of the node to be inserted
-        Node *parent = nullptr;
-        Node *currentNode = nullptr;
-        search(x, parent, currentNode);
+        //step 5: if parent is null (TREE is empty)
+        if(parent == nullptr){
 
-        // step 5: If parent is NULL (Tree is empty)
-        if (parent == nullptr)
-        {
-            // 5a: mark the new node as ROOT
-            ROOT = newNode;
+            //5a : Mark the new node as ROOT
+            ROOT = newnode;
 
-            // 5b: Exit
+            //5b : Exit 
             return;
         }
-        // step 6: If the value in the data field of the node is less than that of parents
-        if (x <parent->info)
-        {
-            // 6a : Make the left child of parent point to the new node
-            parent->leftchild = newNode;
 
-            // 6b : Exit
-            return;
+        //step 6: if the value in data field of new node is less than that of parent 
+        if (x < parent->info){
+
+            //6a :  make the left child of parent point to the new node 
+            parent->leftchild = newnode;
+
+            //6b : Exit 
+            return ;
         }
-        // step 7 : If the value in the data field of the new node is greater than that of the parent
-        else if (x < parent->info)
-        {
-            // 7a : Make the right child of the parent point to the new node
-            parent->rightchild = newNode;
 
-            // 7b : Exit
-            return;
+        //step 7: if the value in data field of new node is greater than that of the parent 
+        else if (x > parent->info){
+            //7a : make the right child of parent point to the new node 
+            parent->rightchild = newnode;
+
+            //7b : exit 
+            return ;
+        } 
+    }
+void search(int element, node *&parent, node *&currentnode)
+    {
+        // This Function searches the currentNode of the specified Node as well as the current Node of its parent
+        currentnode = ROOT;
+        parent = nullptr;
+        while ((currentnode != nullptr) && (currentnode->info != element))
+        {
+            parent = currentnode;
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
         }
     }
+
     
-    
-
-    void inorder(Node *ptr)
+    void inorder(node *ptr)
     {
-        if (isempty())
+        if (isEmpty())
         {
             cout << "Tree is empty" << endl;
             return;
         }
         if (ptr == nullptr)
-        return;
-    inorder(ptr->leftchild);
-    cout << ptr->info<<" "; // parent
-    inorder(ptr->rightchild);
-    }
+            return;
 
-    void preorder(Node *ptr)
+        inorder(ptr->leftchild);
+        cout << ptr->info << " ";
+        inorder(ptr->rightchild);
+        
+    }    
+
+        void preorder(node *ptr)
     {
-        if (isempty())
+        if (isEmpty())
         {
             cout << "Tree is empty" << endl;
             return;
         }
         if (ptr == nullptr)
-        return;
-    cout << ptr->info<<" "; // parent
-    preorder(ptr->leftchild);
-    preorder(ptr->rightchild);
-    }
+            return;
 
-    void posrder(Node *ptr)
+        cout << ptr->info << " ";
+        preorder(ptr->leftchild);
+        preorder(ptr->rightchild);
+        
+    }    
+
+        void postorder(node *ptr)
     {
-        if (isempty())
+        if (isEmpty())
         {
             cout << "Tree is empty" << endl;
             return;
         }
         if (ptr == nullptr)
-        return;
-    posorder(ptr->leftchild);
-    posorder(ptr->rightchild);
-    cout << ptr->info<<" "; // parent
-    }
+            return;
 
-    bool isempty()
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " ";
+    }    
+
+    bool isEmpty()
     {
-    // checks if the tree is empty
-    return ROOT == nullptr;
+        // Checks if the tree is empty
+        return ROOT == nullptr;
     }
+};
 
- };
 
- int main()
- {
+int main(){
+
     BinaryTree x;
     while (true)
     {
@@ -177,6 +189,4 @@ public:
         }
         }
     }
- }
- 
-    
+};
