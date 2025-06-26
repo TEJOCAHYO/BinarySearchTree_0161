b00790af93c895310fac084edf6b8406c2f15007
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-06-26 10:52:52 +0700

    mencari node yang di tentukan

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -31,23 +31,51 @@ public:
         cout << "Masukkan nilai; ";
         cin >> x;
 
-      
-    }
-    
-    void search(int elemnt, Node *&parent, Node *&currentNode)
-    {
-        // this function searches the currentNode of the specified node as well as the current Node of its parent
-        currentNode = ROOT;
-        parent = nullptr;
-        while ((currentNode != nullptr) && (currentNode->info != element))
+        // Step 1; Allocate memory for the new node
+        Node*newNode = new Node();
+
+        // step 2:Assign value to the data field of new node
+        newNode->info = x;
+
+        // step 3: Make the lift and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        // step 4: Locate the node which will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
+
+        // step 5: If parent is NULL (Tree is empty)
+        if (parent == nullptr)
         {
-            parent = currentNode;
-            if (element < currentNode->info)
-                currentNode = currentNode->leftchild;
-            else 
-                currentNode = currentNode->rightchild;
+            // 5a: mark the new node as ROOT
+            ROOT = newNode;
+
+            // 5b: Exit
+            return;
+        }
+        // step 6: If the value in the data field of the node is less than that of parents
+        if (x <parent->info)
+        {
+            // 6a : Make the left child of parent point to the new node
+            parent->leftchild = newNode;
+
+            // 6b : Exit
+            return;
+        }
+        // step 7 : If the value in the data field of the new node is greater than that of the parent
+        else if (x < parent->info)
+        {
+            // 7a : Make the right child of the parent point to the new node
+            parent->rightchild = newNode;
+
+            // 7b : Exit
+            return;
         }
     }
+    
+    
 
     void inorder(Node *ptr)
     {
@@ -91,5 +119,64 @@ public:
     cout << ptr->info<<" "; // parent
     }
 
+    bool isempty()
+    {
+    // checks if the tree is empty
+    return ROOT == nullptr;
+    }
 
+ };
+
+ int main()
+ {
+    BinaryTree x;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5) : ";
+
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            x.insert();
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+
+            x.preorder(x.ROOT);
+            break;
+        }
+        case '4':
+        {
+
+            x.postorder(x.ROOT);
+            break;
+        }
+        case '5':
+            return 0;
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
+ }
+ 
     
